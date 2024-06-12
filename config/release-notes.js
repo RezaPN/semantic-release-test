69cf3634f63aea21f4f0788371e4b5ce2b53d720
RezaPN <rezapratamanu@gmail.com>
2024-06-12 17:12:57 +0700

    update: added release-notes tranform commit

--- config/release-notes.js
+++ config/release-notes.js
@@ -1,17 +1,69 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const conventionalChangelogAngular = require('conventional-changelog-angular');
 
+// eslint-disable-next-line no-undef
 module.exports = conventionalChangelogAngular.then(writerOpts => {
   const originalTransform = writerOpts.writerOpts.transform;
 
-  writerOpts.writerOpts.transform = (commit, context) => {
-    if (commit.type === 'update') {
-      commit.type = 'Updates'; // Set the custom type
+  writerOpts.writerOpts.transform = (commit) => {
+    commit.notes.forEach(note => {
+      note.title = 'BREAKING CHANGES';
+    });
+
+    switch (commit.type) {
+      case 'feat':
+        commit.type = 'Features';
+        break;
+      case 'fix':
+        commit.type = 'Bug Fixes';
+        break;
+      case 'update':
+        commit.type = 'Updates';
+        break;
+      case 'perf':
+        commit.type = 'Performance Improvements';
+        break;
+      case 'revert':
+        commit.type = 'Reverts';
+        break;
+      case 'docs':
+        commit.type = 'Documentation';
+        break;
+      case 'style':
+        commit.type = 'Styles';
+        break;
+      case 'refactor':
+        commit.type = 'Code Refactoring';
+        break;
+      case 'test':
+        commit.type = 'Tests';
+        break;
+      case 'build':
+        commit.type = 'Build System';
+        break;
+      case 'ci':
+        commit.type = 'Continuous Integration';
+        break;
     }
 
-    // Apply the original transform
-    commit = originalTransform(commit, context);
+    if (commit.scope === '*') {
+      commit.scope = '';
+    }
 
-    return commit;
+    if (typeof commit.hash === 'string') {
+      commit.shortHash = commit.hash.substring(0, 7);
+    }
+
+    // Adjust the commit subject format
+    if (commit.scope && commit.scope !== '') {
+      commit.subject = `(${commit.scope}): ${commit.subject}`;
+    }
+
+    if (typeof commit.subject === 'string') {
+      commit.subject = commit.subject.substring(0, 72);
+    }
+
+    return originalTransform(commit);
   };
 
   return writerOpts;
