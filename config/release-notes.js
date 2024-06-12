758c64c418c14f8c8d13aa7b11bb402b703fb4ac
RezaPN <rezapratamanu@gmail.com>
2024-06-12 16:51:13 +0700

    update: change release-notes

--- config/release-notes.js
+++ config/release-notes.js
@@ -5,25 +5,11 @@ module.exports = conventionalChangelogAngular.then(writerOpts => {
 
   writerOpts.writerOpts.transform = (commit, context) => {
     if (commit.type === 'update') {
-      // Add a dummy note to prevent discard
-      const dummyNote = {
-        title: 'dummy',
-        text: 'dummy',
-      };
-      
-      if (!commit.notes.length) {
-        commit.notes = [dummyNote];
-      }
-
-      // Apply the original transform
-      commit = originalTransform(commit, context);
-
-      // Remove the dummy note and set the custom type
-      commit.notes = commit.notes.filter(note => note !== dummyNote);
-      commit.type = 'Updates';
-    } else {
-      commit = originalTransform(commit, context);
+      commit.type = 'Updates'; // Set the custom type
     }
+
+    // Apply the original transform
+    commit = originalTransform(commit, context);
 
     return commit;
   };
